025fa141fe070ccde84c1d985de316fd1e88a53b
Darren Xu <darrenxu1994@gmail.com>
2022-10-29 13:37:57 +1100

    Convert to function app

---
+++ netlify/functions/cookbook.js
@@ -0,0 +1,16 @@
+const { Client } = require("@notionhq/client");
+require("dotenv").config();
+
+const notion = new Client({ auth: process.env.NOTION_API_KEY });
+
+exports.handler = async function (event, context) {
+  const databaseId = "bb40eda301eb4192bd1539286723ed30";
+  const response = await notion.databases.query({
+    database_id: databaseId,
+  });
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ response }),
+  };
+};
